257f17cb409ed1e5264f4bfcc981fb21c809f3d8
MuraliMaddy21 <110399320+MuraliMaddy21@users.noreply.github.com>
2023-02-02 19:41:48 +0530

    export to excel option added

--- src/app/invoice/invoice.component.ts
+++ src/app/invoice/invoice.component.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-
+import * as XLSX from 'xlsx'
 @Component({
   selector: 'app-invoice',
   templateUrl: './invoice.component.html',
@@ -9,49 +9,45 @@ import { Router } from '@angular/router';
 })
 export class InvoiceComponent implements OnInit {
 
-  result:any;
-  items:any;
-  time:any="";
-  filterdata:any=""
+  result: any;
+  items: any;
+  time: any = "";
+  filterdata: any = ""
+  fileName= 'ExcelSheet.xlsx';
 
-  constructor(private http:HttpClient,private route:Router) {
-   }
+  constructor(private http: HttpClient, private route: Router) {
+  }
 
   ngOnInit(): void {
 
-    let date:Date= new Date()
-    this.time=date;
+    let date: Date = new Date()
+    this.time = date;
 
-    this.http.get('http://localhost:3030/getauth',{responseType:'json'}).subscribe((response)=>
-    {
-     console.log(response)
-     this.result=response
-     if(this.result==null)
-     {
-       window.alert("You haven't logged in!Redirecting to Login Page");
-       this.route.navigate([""])
-     }
+    this.http.get('http://localhost:3030/getauth', { responseType: 'json' }).subscribe((response) => {
+      console.log(response)
+      this.result = response
+      if (this.result == null) {
+        window.alert("You haven't logged in!Redirecting to Login Page");
+        this.route.navigate([""])
+      }
     })
 
-    this.http.get('http://localhost:3030/cpinvoice',{responseType:'json'}).subscribe((response)=>
-   {
-    console.log(response)
-    this.result=response
-    this.items=this.result['Envelope']['Body']['ZFM_INVOICE_CP_MDResponse']['IT_INVOICE']['item']
-    console.log(this.items)
-    
-
-})
-}
+    this.http.get('http://localhost:3030/cpinvoice', { responseType: 'json' }).subscribe((response) => {
+      console.log(response)
+      this.result = response
+      this.items = this.result['Envelope']['Body']['ZFM_INVOICE_CP_MDResponse']['IT_INVOICE']['item']
+      console.log(this.items)
 
 
-  shutdown()
-  {
-    this.http.get('http://localhost:3030/shutdown',{responseType:'json'}).subscribe((data)=>
-    {
-      
+    })
+  }
+
+
+  shutdown() {
+    this.http.get('http://localhost:3030/shutdown', { responseType: 'json' }).subscribe((data) => {
+
     });
-    
+
     this.route.navigate([""]);
   }
 
@@ -60,7 +56,14 @@ export class InvoiceComponent implements OnInit {
       console.log(data);
     });
   }
-  
+
+  exportexcel(): void {
+    let element = document.getElementById('excel-table'); 
+    const ws: XLSX.WorkSheet = XLSX.utils.table_to_sheet(element);
+     const wb: XLSX.WorkBook = XLSX.utils.book_new(); 
+     XLSX.utils.book_append_sheet(wb, ws, 'Sheet1'); 
+     XLSX.writeFile(wb, this.fileName); }
+
 }
 
 
